7ff21c5c2d1f2d26df1d323b5371f7373577f8c9
Paulo <paulo_sanches2012@hotmail.com>
2019-04-24 22:41:43 -0300

    Jogo Game of Thrones

---
+++ src/app/app.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { GOTService } from './got.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  tela = 'inicial';
+  jogoAvancarContador = 0;
+  nome: string = null;
+  casa: string = null;
+  personagem: string = null;
+
+  constructor(private service: GOTService){
+
+  }
+
+  inicialAvancar() {
+    this.tela = 'jogo';
+    this.service.salvarJogador(this.nome);
+  }
+
+  jogoAvancar() {
+    this.jogoAvancarContador++;
+    this.service.salvarEscolha(this.nome, this.casa, this.personagem);
+    this.casa = null;
+    this.personagem = null;
+    if (this.jogoAvancarContador === 3) {
+      this.tela = 'resultado';
+    }
+  }
+
+  reiniciar() {
+    this.tela = 'inicial';
+    this.jogoAvancarContador = 0;
+    this.nome = null;
+  }
+}
